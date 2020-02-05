581505d5c68fbd59907406310c41e28129087796
Zac Smith <zrs3141592@gmail.com>
2020-02-04 21:53:29 -0700

    update index.js to use database
    Make the index use async/await and add in the database

--- src/index.js
+++ src/index.js
@@ -7,17 +7,29 @@ const morgan = require("morgan");
 const app = express();
 
 const ads = [{ title: "Hello, world (again)!" }];
+const { startDatabase } = require("./database/mongo.js");
+const { insertAd, getAds } = require("./database/ads.js");
 
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan("combined"));
 
-app.get("/", (req, res) => {
-  res.send(ads);
+// app.get("/", (req, res) => {
+//   res.send(ads);
+// });
+
+app.get("/", async (req, res) => {
+  res.send(await getAds());
+});
+
+startDatabase().then(async () => {
+  await insertAd({
+    title: "Hello, now from the in-memory database!"
+  });
 });
 
 const PORT = 5000;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`\n ** Server is listening on port ${PORT} **\n`);
 });
