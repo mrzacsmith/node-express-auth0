2e1cb00db34db7635141a55789d954ffbcff5f3d
Zac Smith <zrs3141592@gmail.com>
2020-02-04 22:00:47 -0700

    refactor to arrow functions

--- src/database/ads.js
+++ src/database/ads.js
@@ -2,21 +2,37 @@ const { getDatabase } = require("./mongo.js");
 
 const collectionName = "ads";
 
-async function insertAd(ad) {
+// async function insertAd(ad) {
+//   const database = await getDatabase();
+//   const { insertedId } = await database
+//     .collection(collectionName)
+//     .insertOne(ad);
+//   return insertedId;
+// }
+
+const insertAd = async ad => {
   const database = await getDatabase();
   const { insertedId } = await database
     .collection(collectionName)
     .insertOne(ad);
   return insertedId;
-}
+};
 
-async function getAds() {
+const getAds = async () => {
   const database = await getDatabase();
   return await database
     .collection(collectionName)
     .find({})
     .toArray();
-}
+};
+
+// async function getAds() {
+//   const database = await getDatabase();
+//   return await database
+//     .collection(collectionName)
+//     .find({})
+//     .toArray();
+// }
 
 module.exports = {
   insertAd,
